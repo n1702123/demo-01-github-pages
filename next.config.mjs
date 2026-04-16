83266bc4de1bd81cd276e583ed8e54a3a6c090dd
Allen Juan <allen.juan@webcomm.com.tw>
2026-04-17 00:20:10 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',         // 輸出靜態檔案，讓 GitHub Pages 可以讀
-  basePath: '/hello-world', // 對應 GitHub repo 名稱
+  basePath: '/demo-01-github-pages', // 對應 GitHub repo 名稱
   images: {
     unoptimized: true,      // 靜態輸出不支援 Next.js Image Optimization
   },
